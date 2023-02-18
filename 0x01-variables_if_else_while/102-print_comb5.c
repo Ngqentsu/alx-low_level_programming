9291700e58ee1b50c86a4ae2b9ff86fcfcb19201
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-02-17 19:03:33 -0800

    program that prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,7 @@ int main(void)
 {
 int num1;
 int num2 = 0;
- 
+
 int num11;
 int num22 = 0;
 
@@ -17,7 +17,7 @@ while (num2 < 100 && num22 < 100)
 {
 num1 = 0;
 num11 = 0;
- 
+
 while (num1 < 100 && num11 < 100)
 {
 if (num2 != num1 && num2 < num1 && num22 != num11 && num22 < num11)
