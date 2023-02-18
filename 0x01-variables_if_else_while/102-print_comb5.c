18de568e4699b85f2b3ea8bf7772b2ffce2fd24e
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-02-17 18:07:02 -0800

    program that prints all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/**                                                                                                                                                                     
+ * main - program that prints all possible different combinations of 3 digits                                                                                           
+ *                                                                                                                                                                      
+ * Return: Always 0 (Success)                                                                                                                                           
+ */
+int main(void)
+{
+int num1;
+int num2;
+int num3 = 0;
+
+int num11;
+int num22;
+int num33 = 0;
+ 
+while (num1 <= 98)
+{
+num2 = (num1/10 * '0');
+num3 = (num1%10 * '0');
+num11 = 0;
+
+while (num11 <= 99)
+{
+num22 = (num11/10 * '0');
+num33 = (num11%10 * '0');
+
+if (num1 < num11)
+{
+putchar(num2);
+putchar(num3);
+putchar(' ');
+putchar(num22);
+putchar(num33);
+
+if (num1 != 98)
+{
+putchar(',');
+putchar(' ');
+}
+}
+num11++;
+}
+num1++;
+}
+putchar('\n');
+return (0);
+}
