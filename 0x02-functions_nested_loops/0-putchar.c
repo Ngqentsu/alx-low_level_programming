5c4e4ff58178526d5d125b64b283b8640b6c8bbe
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-02-21 06:46:13 -0800

    program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * main - program that prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+_pitchar('_');
+_pitchar('p');
+_pitchar('u');
+_pitchar('t');
+_pitchar('c');
+_pitchar('h');
+_pitchar('a');
+_pitchar('\n');
+return (0);
+}
