eae82b9ebce340b504671bb8f7b84509d3c122f8
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-02-27 15:17:57 -0800

    a program that generates random valid passwords for the program 101-crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,14 +4,14 @@
 
 /**
  * main - generates random valid passwords for the program
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 int i, n, sum;
 int pass[100];
-sum - 0;
+sum = 0;
 
 srand(time(NULL));
 
@@ -19,9 +19,9 @@ for (i = 0; i < 100; i++)
 {
 pass[i] = rand() % 78;
 sum += pass[i] + '0';
-putchar(pass[i) + '0');
+putchar(pass[i] + '0');
 
-if ((2772 - sum) - '0' < 78)
+if ((2772 - sum) -'0' < 78)
 {
 n = 2772 - '0' - sum;
 sum += n;
