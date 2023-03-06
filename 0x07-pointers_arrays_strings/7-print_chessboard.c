cef1d70626531f4320b4321279590e3b26a18c11
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-03-06 15:27:41 -0800

    a function that prints the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,7 @@
 
 /**
  * print_chessboard - prints the chessboard
- * @a: the chessboard
+ * @a: the rows
  */
 void print_chessboard(char (*a)[8])
 {
@@ -12,8 +12,11 @@ for (i = 0; i < 8; i++)
 {
 for (j = 0; j < 8; j++)
 {
+if (j == 7)
 _putchar(a[i][j]);
 _putchar('\n');
+else
+_putchar(a[i][j]);  
 }
 }
 }
