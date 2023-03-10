4d7f6c99281755739848090ddf721dc671976835
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-03-10 11:36:02 -0800

    a program that prints all arguments it receives

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * @argv: argument vector
  * Return: Always 0 (Success)
  */
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
 (void) argc;
 printf("%d\n", *argv[0]);
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,14 +2,15 @@
 #include "main.h"
 
 /**
- * main - prints the number of arguments
+ * main - prints all arguments it receives
  * @argc: argument count
  * @argv: the argument
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-(void) argv;
-printf("%d\n", argc - 1);
+int i;
+for (i = 0; i < argc; i++)
+printf("argv[%d] = %s\n", i, argv[i]);
 return (0);
 }
