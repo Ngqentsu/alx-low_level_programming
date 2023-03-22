426e606490aa1e01119507ecd66e8eda801d1f15
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-03-22 10:38:16 -0700

    a program that prints the opcodes of its own main function

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - contain your main function
+ * @argc: The number of arguments
+ * @argv: An array
+ * Return: Always 0 (Success)
+ */
+
