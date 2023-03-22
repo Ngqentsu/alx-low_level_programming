b64fbf7f93f5a27fe02b722253ff44a42d49409d
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-03-22 12:48:17 -0700

    Update

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * struct op - struct op
@@ -14,7 +13,7 @@ typedef struct op
 {
 char *op;
 int (*f)(int a, int b);
-} op_t;
+}op_t;
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,16 +9,16 @@
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL},
-};
+			{"+", op_add},
+			{"-", op_sub},
+			{"*", op_mul},
+			{"/", op_div},
+			{"%", op_mod},
+			{NULL, NULL},
+		};
 
 int j;
-j =0;
+j = 0;
 
 while (ops[j].op != NULL && *(ops[j].op) != *s)
 {
