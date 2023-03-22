46e085d192df8239acae3c16d1fbd97eb4dfab3a
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-03-22 09:25:04 -0700

    a function that executes a function given as a parameter on each element of an array

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,7 +5,7 @@
  * print_name - a function that prints a name
  * @name: string
  * @f: pointer to function
- **/
+ */
 void print_name(char *name, void (*f)(char *))
 {
 if (name == NULL || f == NULL)
