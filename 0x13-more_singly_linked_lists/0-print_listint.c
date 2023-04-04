75da66a5172c695fbef8bd63675f5abe0340fe93
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-04-04 03:24:53 -0700

    a function that prints all the elements of a listint_t list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,6 @@
 /**
  * print_listint - prints all the elements of a linked list
  * @h: linked list of type listint_t to print
- *
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
@@ -12,8 +11,8 @@ size_t nodes = 0;
 
 while (h)
 {
-printf("%d\n", (*h).n);
 nodes++;
+printf("%d\n", (*h).n);
 h = (*h).next;
 }
 return (nodes);
