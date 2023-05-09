3ca53a615132a6e60f05830363b56586675e0fdd
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-05-08 20:02:38 -0700

    a program that copies the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: The number of arguments
+ * @argv: An array of pointers
+ * Return: 0 Always (Success)
+ */
+int main(int argc, char *argv[])
+{
+char *buffer;
+int rd, fd, op, wrt;
+
+if (argc != 3)
+{
+fprintf(stderr, "Usage: cp file_from file_to\n");
+exit(97);
+}
+
+op = open(argv[1], O_RDONLY);
+fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+rd = read(op, buffer, 1024);
+wrt = write(fd, buffer, rd);
+if (op == -1 || rd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+free(buffer);
+exit(98);
+}
+
+if (fd == -1 || wrt == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write into file %s\n", argv[1]);
+free(buffer);
+exit(99);
+}
+
+close(op);
+close(fd);
+
+return (0);
+}
