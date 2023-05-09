6a52da7bbd5030f927600a40e59d61ee016820da
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-05-08 20:23:15 -0700

    a program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,7 +14,7 @@ int rd, fd, op, wrt;
 
 if (argc != 3)
 {
-fprintf(stderr, "Usage: cp file_from file_to\n");
+fprintf(stderr, "Usage: cp file_op file_fd\n");
 exit(97);
 }
 
@@ -22,20 +22,43 @@ op = open(argv[1], O_RDONLY);
 fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 rd = read(op, buffer, 1024);
 wrt = write(fd, buffer, rd);
-if (op == -1 || rd == -1)
+if (op == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+exit(98);
+}
+
+if (fd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write into file %s\n", argv[1]);
+exit(99);
+}
+
+buffer = malloc(1024 * sizeof(char));
+if (buffer == NULL)
+{
+dprintf(STDERR_FILENO, "Error: Failed to allocate memory\n");
+exit(100);
+}
+
+while (rd > 0)
+{
+if (wrt != rd)
+{
+dprintf(STDERR_FILENO, "Error: Failed to write to file %s\n", argv[2]);
+free(buffer);
+exit(99);
+}
+}
+
+if (rd < 0)
+{
+dprintf(STDERR_FILENO, "Error: Failed to write to file %s\n", argv[2]);
 free(buffer);
 exit(98);
 }
 
-if (fd == -1 || wrt == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write into file %s\n", argv[1]);
 free(buffer);
-exit(99);
-}
-
 close(op);
 close(fd);
 
