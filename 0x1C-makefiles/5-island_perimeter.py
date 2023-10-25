da5fc4d908359fab637b3b12e331ca6a98bb2f04
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-10-25 08:39:00 -0700

    A function that returns the perimeter of the island described in grid

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A function that determines the perimeter of an island."""
+
+
+def island_perimeter(grid):
+    """Calculates the perimiter of the island.
+
+    Args:
+        grid (list): A list of list of integers.
+    """
+    height = len(grid[0])
+    width = len(grid)
+    count = 0
+    size = 0
+
+    for i in range(width):
+        for j in range(height):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                   count += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    count += 1
+    perimeter = (size * 4) - (count * 2)
+    return perimeter
