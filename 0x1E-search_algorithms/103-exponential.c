9bd7d443ef19c443a023689003eb0e58969e512b
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-12-13 15:38:28 -0800

    function that searches for a value in a sorted array of integers using the Exponential search algorithm

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -12,17 +12,17 @@ int exponential_search(int *array, size_t size, int value)
 {
 	size_t i = 0, right;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	if (array[0] != value)
 	{
-		for (i = 1; i < size && array[i] <= value; i = i * 2)
+		for (i = 1; i < size && array[i] <= value; i *= 2)
 			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	}
 
 	right = i < size ? i : size - 1;
 	printf("Value found between indexes [%ld] and [%ld]\n", i / 2, right);
 
-	return (binary_search(array, i / 2, right, value));
+	return (binary_search(array, right - i / 2 + 1, value));
 }
