dd881fc702d786a86901c5e947b77e6afa6dbea9
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-12-13 16:24:37 -0800

    A function that searches for a value in a sorted skip list of integers.

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -14,38 +14,28 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *node, *jump;
 
-	/* Check if the list is NULL */
 	if (list == NULL)
 		return (NULL);
 
-	/* Perform linear skip */
 	for (node = jump = list; jump->next != NULL && jump->n < value;)
 	{
-		/* Store the previous jump node */
 		node = jump;
 
-		/* If the express lane is available */
 		if (jump->express != NULL)
 		{
-			/* Jump to the next express node */
 			jump = jump->express;
 
 			printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
 		}
 		else
 		{
-			/* Traverse to the end of the list */
 			while (jump->next != NULL)
-				/* Move to the next node in the list */
 				jump = jump->next;
 		}
 	}
 
 	printf("Value found between indexes [%ld] and [%ld]\n",
 			node->index, jump->index);
-
-	/*  performs a linear search, checking each */
-	/*  node's value to find the target value */
 	for (; node->index < jump->index && node->n < value; node = node->next)
 		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
 
